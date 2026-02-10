309906d83186df94f62830449b9d94d65cc51209
Lyndon Arthur Puzon <mrdonpuz@gmail.com>
2026-02-09 21:48:30 -0500

    Update solution for same-tree

--- leetcode/same-tree.py
+++ leetcode/same-tree.py
@@ -6,37 +6,47 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # level order traversal and compare each node
+        # # iterative bfs / level order solution
+        # # level order traversal and compare each node
 
-        # separate queues?
-        p_queue = [p]
-        q_queue = [q]
+        # # separate queues?
+        # p_queue = [p]
+        # q_queue = [q]
 
-        p_visited = set()
-        q_visited = set()
+        # p_visited = set()
+        # q_visited = set()
 
-        while p_queue and q_queue:
-            p_curr = p_queue.pop(0)
-            q_curr = q_queue.pop(0)
+        # while p_queue and q_queue:
+        #     p_curr = p_queue.pop(0)
+        #     q_curr = q_queue.pop(0)
 
-            if p_curr and q_curr and p_curr.val != q_curr.val:
-                return False
+        #     if p_curr and q_curr and p_curr.val != q_curr.val:
+        #         return False
 
-            if (p_curr and not q_curr) or (not p_curr and q_curr):
-                return False
+        #     if (p_curr and not q_curr) or (not p_curr and q_curr):
+        #         return False
             
-            if p_curr:
-                p_visited.add(p_curr)
-                p_queue.append(p_curr.left)
-                p_queue.append(p_curr.right)
+        #     if p_curr:
+        #         p_visited.add(p_curr)
+        #         p_queue.append(p_curr.left)
+        #         p_queue.append(p_curr.right)
     
-            if q_curr:
-                q_visited.add(q_curr)
-                q_queue.append(q_curr.left)
-                q_queue.append(q_curr.right)
+        #     if q_curr:
+        #         q_visited.add(q_curr)
+        #         q_queue.append(q_curr.left)
+        #         q_queue.append(q_curr.right)
         
 
-        if p_queue or q_queue:
-            return False
+        # if p_queue or q_queue:
+        #     return False
         
-        return True
+        # return True
+
+        # dfs solution
+        if not p and not q:
+            return True
+        
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        return False
